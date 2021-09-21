287d7ac482ec6b2ad8f36a408717c598afee267e
Augusto <augustoamaro@icloud.com>
2021-09-21 11:30:34 -0300

    Added date examples

---
+++ 03_Day_Booleans_Operators_Date/date.js
@@ -0,0 +1,31 @@
+// No JavaScript, a hora e a data atuais são criadas usando o objeto date. Todos os métodos são iniciados com a palavra get.
+// getFullYear(), getMonths(), getDate(), getDay(), getHours(), getMinutes(), getSeconds(), getMilliseconds(), getTime(), getDay()
+
+// Criando um objeto Date()
+const now = new Date();
+console.log(now)
+
+// Criando um objeto de tempo podemos acessar qualquer informação de data e hora do objeto usando os métodos get.
+
+// Extrair ou obter o ano inteiro de Date()
+const year = new Date();
+console.log(year.getFullYear()); // 2021
+
+// Obtendo o mês
+const month = new Date();
+console.log(month.getMonth()) // Setembro - 08 porque Janeiro é 0
+
+// Obtendo o dia
+const day = new Date();
+console.log(day.getDate()) // 21
+
+// Obtendo horas
+const hour = new Date();
+console.log(hour.getHours());
+
+// Obtendo minutos
+const minutes = new Date();
+console.log(hour.getMinutes());
+
+const seconds = new Date();
+console.log(hour.getSeconds()); 
--- 03_Day_Booleans_Operators_Date/ternaryOperator.js
+++ 03_Day_Booleans_Operators_Date/ternaryOperator.js
@@ -11,3 +11,15 @@ isRaining = false;
 isRaining
     ? console.log('Você precisa de uma capa de chuva.')
     : console.log('Não há necessidade de capa de chuva.')
+
+
+    let number = 5
+        number > 0
+            ? console.log(`O ${number} é um número positivo`)
+            : console.log(`O ${number} é um número negativo`)
+        number = - 5
+
+        number > 0
+            ? console.log(`O ${number} é um número positivo`)
+            : console.log(`O ${number} é um número negativo`)
+    
