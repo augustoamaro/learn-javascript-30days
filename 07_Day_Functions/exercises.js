170b51a285e5280654c1886bca99d93f33dfd2c8
Augusto <augustoamaro@icloud.com>
2021-09-30 11:10:03 -0300

    Added list of exercises in function examples

---
+++ 07_Day_Functions/exercises.js
@@ -0,0 +1,39 @@
+// Declare uma função fullName e imprima seu nome completo.
+
+// Declare uma função fullName e agora ela leva firstName, lastName como um parâmetro e retorna seu nome completo.
+
+// Declare uma função addNumbers e ela leva dois dois parâmetros e retorna sum.
+
+// Uma área de um retângulo é calculada da seguinte maneira: área = comprimento x largura . Escreva uma função que calcule areaOfRectangle .
+
+// O perímetro de um retângulo é calculado da seguinte maneira: perímetro = 2x (comprimento + largura) . Escreva uma função que calcule perimeterOfRectangle .
+
+// O volume de um prisma retangular é calculado da seguinte maneira: volume = comprimento x largura x altura . Escreva uma função que calcule volumeOfRectPrism .
+
+// A área de um círculo é calculada da seguinte maneira: área = π xrxr . Escreva uma função que calcule areaOfCircle
+
+// A circunferência de um círculo é calculada da seguinte maneira: circunferência = 2πr . Escreva uma função que calcule circumOfCircle
+
+// A densidade de uma substância é calculada da seguinte maneira: densidade = massa / volume . Escreva uma função que calcule a densidade .
+
+// A velocidade é calculada dividindo a distância total percorrida por um objeto em movimento pela quantidade total de tempo gasto. Escreva uma função que calcule a velocidade de um objeto em movimento, velocidade .
+
+// O peso de uma substância é calculado da seguinte maneira: peso = massa x gravidade . Escreva uma função que calcule o peso .
+
+// A temperatura em oC pode ser convertida em oF usando esta fórmula: oF = (oC x 9/5) + 32 . Escreva uma função que converte oC em oF convertCelciusToFahrenheit .
+
+// O índice de massa corporal (IMC) é calculado da seguinte forma: bmi = peso em Kg / (altura x altura) em m2 . Escreva uma função que calcule bmi . O IMC é usado para definir amplamente diferentes grupos de peso em adultos de 20 anos ou mais. Verifique se uma pessoa está abaixo do peso, normal, com sobrepeso ou obeso com base nas informações fornecidas abaixo.
+
+// Os mesmos grupos se aplicam a homens e mulheres.
+// Baixo peso : o IMC é inferior a 18,5
+// Peso normal : o IMC é de 18,5 a 24,9
+// Sobrepeso : o IMC é de 25 a 29,9
+// Obeso : o IMC é 30 ou mais
+// Escreva uma função chamada checkSeason , ela pega um parâmetro de mês e retorna a estação: outono, inverno, primavera ou verão.
+
+// Math.max retorna seu maior argumento. Escreva uma função findMax que receba três argumentos e retorne seu máximo sem usar o método Math.max.
+
+// console . log ( findMax ( 0 ,  10 ,  5 ) ) 
+// 10 
+// console . log ( findMax ( 0 ,  - 10 ,  - 2 ) ) 
+// 0
