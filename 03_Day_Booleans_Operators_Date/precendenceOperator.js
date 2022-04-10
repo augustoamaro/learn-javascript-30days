0e7a87b1c0a31bd5525a52e1fdc0528871c40106
Augusto <augustoamaro@icloud.com>
2022-04-10 01:25:17 -0300

    Added precedence operator

--- 03_Day_Booleans_Operators_Date/precendenceOperator.js
+++ 03_Day_Booleans_Operators_Date/precendenceOperator.js
@@ -1,1 +1,6 @@
-// Fazer
+// Determina a ordem em que os operadores são processados. Operadores com maior precedência são processados primeiro.
+
+const resultado = 3 + 4 * 5; // 23
+console.log(resultado)
+
+// Verificar tabela de precedencia.
